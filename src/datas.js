a8aba012cddc4be693f4ffb58907fac2223eae3f
masome hosine <hosine.masome@gmail.com>
2023-10-09 23:32:11 +0330

    add datas in data file and export and user in components

---
+++ src/datas.js
@@ -0,0 +1,214 @@
+let xAxisData = [
+  {
+    name: 'Jan',
+    Sale: 112_000,
+  },
+  {
+    name: 'Feb',
+    Sale: 99_000,
+  },
+  {
+    name: 'Mar',
+    Sale: 12_000,
+  },
+  {
+    name: 'Apr',
+    Sale: 99_000,
+  },
+  {
+    name: 'May',
+    Sale: 54_000,
+  },
+  {
+    name: 'Jun',
+    Sale: 85_000,
+  },
+  {
+    name: 'Jul',
+    Sale: 35_000,
+  },
+  {
+    name: 'Agu',
+    Sale: 18_500,
+  },
+  {
+    name: 'Sep',
+    Sale: 0,
+  },
+  {
+    name: 'Oct',
+    Sale: 73_078,
+  },
+  {
+    name: 'Nov',
+    Sale: 12_000,
+  },
+  {
+    name: 'jan',
+    Sale: 112_000,
+  },
+  {
+    name: 'Dev',
+    Sale: 97_000,
+  },
+]
+
+const newMembers = [
+  {
+    id: 1,
+    username: 'Mohamad Amin',
+    title: 'Web Developer',
+    img: 'images/face-2.png',
+  },
+  {
+    id: 2,
+    username: 'Fateme Hosine ',
+    title: 'Disiner',
+    img: 'images/face-2.png',
+  },
+  {
+    id: 3,
+    username: 'Masome Hosine',
+    title: 'Haker',
+    img: 'images/face-2.png',
+  },
+  {
+    id: 4,
+    username: 'Mahbobe Javadi',
+    title: 'Modir Project',
+    img: 'images/face-2.png',
+  },
+]
+
+const transactions = [
+  {
+    id: 1,
+    customer: 'Mohamd Mohamadi',
+    data: '12 Jun 2022',
+    amount: 123,
+    status: 'Declined',
+    img: 'images/face-2.png',
+  },
+  {
+    id: 2,
+    customer: 'Mahsa Mohamadi',
+    data: '15 May 2023',
+    amount: 253,
+    status: 'Approved',
+    img: 'images/face-2.png',
+  },
+  {
+    id: 3,
+    customer: 'Masome Hosine',
+    data: '14 Set 2022',
+    amount: 556,
+    status: 'Pending',
+    img: 'images/face-2.png',
+  },
+  {
+    id: 4,
+    customer: 'Masam',
+    data: '2 Feb 2023',
+    amount: 698,
+    status: 'Pending',
+    img: 'images/face-2.png',
+  },
+]
+
+const userRows = [
+  {
+    id: 1,
+    usernmae: 'Solmas Alipor',
+    avatar: 'images/face-2.png',
+    status: 'active',
+    transactions: '$55.98',
+    email: 'hosine.masome@gmail.com',
+  },
+  {
+    id: 2,
+    usernmae: 'Zahra Gasemi',
+    avatar: 'images/face-2.png',
+    status: 'active',
+    transactions: '$58.36',
+    email: 'hosine.masome@gmail.com',
+  },
+  {
+    id: 3,
+    usernmae: 'Maede Hosine',
+    avatar: 'images/face-2.png',
+    status: 'active',
+    transactions: '$24.58',
+    email: 'hosine.masome@gmail.com',
+  },
+  {
+    id: 4,
+    usernmae: 'Hadise Hosine',
+    avatar: 'images/face-2.png',
+    status: 'active',
+    transactions: '$44.96',
+    email: 'hosine.masome@gmail.com',
+  },
+  {
+    id: 5,
+    usernmae: 'Mohadese hosine',
+    avatar: 'images/face-2.png',
+    status: 'active',
+    transactions: '$56.98',
+    email: 'hosine.masome@gmail.com',
+  },
+]
+
+let products = [
+  {
+    id: 1,
+    title: 'Asuse',
+    avatar: 'images/face-2.png',
+    price: 890,
+  },
+  {
+    id: 2,
+    title: 'lenovo',
+    avatar: 'images/face-2.png',
+    price: 963,
+  },
+  {
+    id: 3,
+    title: 'appel',
+    avatar: 'images/face-2.png',
+    price: 200,
+  },
+  {
+    id: 4,
+    title: 'aser',
+    avatar: 'images/face-2.png',
+    price: 565,
+  },
+  {
+    id: 5,
+    title: 'sumsong',
+    avatar: 'images/face-2.png',
+    price: 504,
+  },
+  {
+    id: 6,
+    title: 'Hoavi',
+    avatar: 'images/face-2.png',
+    price: 255,
+  },
+]
+
+const productsData = [
+  {
+    name: 'Fab',
+    sale: 5000,
+  },
+  {
+    name: 'Jun',
+    sale: 3000,
+  },
+  {
+    name: 'Mar',
+    sale: 4000,
+  },
+]
+export { xAxisData, newMembers, transactions, userRows, products, productsData }
